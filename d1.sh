45a0af80fbb703514938264a3ea322345f1ee40d
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-01-29 14:29:22 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ./data.csv | cut -f1,6 -d ";" | sort | uniq |cut -f2 -d ";" | sort | uniq -c |sort -nr | head -10 > ./temp/result_d1.txt
+cat ./data.csv | cut -f1,6 -d ";" | sort | uniq | cut -f2 -d ";" | sort | uniq -c | sort -nr | head -10 | awk '{print $2" "$3";" $1}'> ./temp/result_d1.txt
