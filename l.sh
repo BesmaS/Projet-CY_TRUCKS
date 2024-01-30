27913a3567a52196dd41abddb0b001d6ba9aed72
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-01-30 14:53:00 +0100

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Utilisation d'AWK pour traiter le fichier CSV
 awk -F';' '{
-    somme[$1] += $5     
+    sum[$1] += $5     
 }
 END {
-    for (id in somme)   
-    print id ";" somme[id]  
+    for (id in sum)   
+    print id ";" sum[id]  
 }' data.csv | sort -t ';' -k2nr | head -n 10 | sort -t ';' -k1n > temp/result_l.txt
