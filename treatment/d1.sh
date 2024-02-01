3d6aa8848baf482054e6bde0012172ce56e84be7
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-01 12:51:21 +0100

    Update d1.sh

--- treatment/d1.sh
+++ treatment/d1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+chmod 777 data/data.csv
 
-cat ./data.csv | cut -f1,6 -d ";" | sort | uniq | cut -f2 -d ";" | sort | uniq -c | sort -nr | head -10 | awk '{print $2" "$3";" $1}'> ./temp/result_d1.txt
+cat ./data/data.csv | cut -f1,6 -d ";" | sort | uniq | cut -f2 -d ";" | sort | uniq -c | sort -nr | head -10 | awk '{print $2" "$3";" $1}'> ./temp/result_d1.txt
 
-gnuplot ./d1gnu.gnu
+cat temp/result_d1.txt
