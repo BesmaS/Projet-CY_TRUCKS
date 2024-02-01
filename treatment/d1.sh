8f644606db39da903f0c98fd525b4cbca8959803
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-02 00:43:32 +0100

    Delete treatment/d1.sh

--- treatment/d1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod 777 data/data.csv
-
-cat ./data/data.csv | cut -f1,6 -d ";" | sort | uniq | cut -f2 -d ";" | sort | uniq -c | sort -nr | head -10 | awk '{print $2" "$3";" $1}'> ./temp/result_d1.txt
-
-cat temp/result_d1.txt
