ed0f5b44c765ce236700c3d703b0d0810ebd6d53
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-02 12:58:48 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -2,3 +2,7 @@
 chmod 777 data/data.csv
 
 cat data/data.csv | cut -f1,6 -d ";" | sort | uniq | cut -f2 -d ";" | sort | uniq -c | sort -nr | head -10 | awk '{print $2" "$3";" $1}'> temp/result_d1.txt
+
+gnuplot gnuplot/histo_d1.txt
+convert H_d1.png -rotate 90 H_d1.png
+mv H_d1.png images/
