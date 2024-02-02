41696f82afa19a82194a33443178b2101868cd39
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-02 13:20:43 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -4,8 +4,9 @@ chmod 777 data/data.csv
 cut -d ';' -f1,2,5 data/data.csv >temp/s1.csv
 tail -n +2 temp/s1.csv > temp/s2.csv
 
-gcc -o progc/s progc/s.c 
+gcc -o progc/s progc/s.c
 ./progc/s temp/s2.csv
 
-head -n 50 temp/sortie.csv > temp/s.csv
-cat temp/s.csv
+head -n 50 temp/sortie.csv > temp/s.txt
+gnuplot gnuplot/histo_s.txt
+mv H_s.png images/
