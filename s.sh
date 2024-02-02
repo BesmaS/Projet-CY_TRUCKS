6559a177eb17d358a1f3ca52f3341b4374b68b91
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-02 01:44:07 +0100

    Add files via upload

---
+++ s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chmod 777 data/data.csv
+
+cut -d ';' -f1,2,5 data/data.csv >temp/s1.csv
+tail -n +2 temp/s1.csv > temp/s2.csv
+
+gcc -o progc/s progc/s.c 
+./progc/s temp/s2.csv
+
+head -n 50 temp/sortie.csv > temp/s.csv
+cat temp/s.csv
---
+++ t.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+chmod 777 data/data.csv
+	
+	#awk: Cree un fichier avec les villes, le nombre de traversé et de depart depuis $data
+	awk -F';' '
+		NR > 1 {
+			if(!seen[$1,$3]++) town_a[$3]++;
+			if(!seen[$1,$4]++) town_b[$4]++;
+			if($2==1 && !depart_seen[$1,$3]++) depart[$3]++;
+		}
+		END {
+			for(town in town_a)
+				print town ";" town_a[town]+town_b[town] ";" (town in depart ? depart[town] : 0)
+		}
+	' data/data.csv > tmp.txt
+
+	mv tmp.txt temp
+
+	gcc -o progc/t progc/t.c
+	./progc/t
