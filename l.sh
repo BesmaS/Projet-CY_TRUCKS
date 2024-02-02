0fef5100982e76c007a02e41111e3ec2d5977534
BesmaS <131924176+BesmaS@users.noreply.github.com>
2024-02-02 13:12:38 +0100

    Update l.sh

--- l.sh
+++ l.sh
@@ -2,10 +2,12 @@
 chmod 777 data/data.csv
 #Utilisation d'AWK pour traiter le fichier CSV
 awk -F';' '{
-    sum[$1] += $5     
+    sum[$1] += $5    
 }
 END {
-    for (id in sum)   
+    for (id in sum)  
     print id ";" sum[id]  
 }' data/data.csv | sort -t ';' -k2nr | head -n 10 | sort -t ';' -k1n > temp/result_l.txt
-cat temp/result_l.txt
+
+gnuplot gnuplot/histo_l.txt
+mv H_l.png images/
